024cfdf357354ae78075540ab39007c2451dcf80
Ethan Mick <ethan@ethanmick.com>
2023-03-29 13:22:11 -0400

    Initial commit from Create Next App

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client'
+import { hash } from 'bcrypt'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const password = await hash('test', 12)
+  const user = await prisma.user.upsert({
+    where: { email: 'test@test.com' },
+    update: {},
+    create: {
+      email: 'test@test.com',
+      name: 'Test User',
+      password
+    }
+  })
+  console.log({ user })
+}
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
